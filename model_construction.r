9547b6e87ccd3db290d52a5112ab587f69e5af1a
tnh-fsa <52284462+tnh-fsa@users.noreply.github.com>
2020-01-20 16:26:06 +0000

    Update model_construction.r
    Build and fit Bayesian network.

--- model_construction.r
+++ model_construction.r
@@ -1,5 +1,8 @@
 # Non-UK RASFFs model construction.
 library(lubridate)
+library(bnlearn)
+library(bnviewer)
+library(caret)
 source('helpers.r')
 
 # Number of days considered to be the near-term.
@@ -72,7 +75,7 @@ df_full <- dplyr::left_join(
       false=dist_uk<-0)
     ) %>%
   dplyr::left_join(x=., y=unique(df_hazards_raw), by='reference') %>%
-  dplyr::mutate(uk_report_soon=0)
+  dplyr::mutate(uk_rasff_soon=0)
 # Find the UK report days and  associated near-term window.
 df_uk_report_days <- df_full %>%
   dplyr::filter(notifyingCountry=='United Kingdom') %>%
@@ -90,23 +93,25 @@ for (r in 1:dim(df_uk_report_days)[1]){
           right=df_uk_report_days$days_from_start[r]
           )
       ) %>%
-    dplyr::mutate(df_uk_report_soon=1)
-  references_uk_soon <- c(references_uk_soon,
-                          dplyr::pull(df_filter, var=reference))
-}
-# Mutate uk_report_soon to reflect imminent UK RASFFs and filter out.
+    dplyr::mutate(df_uk_rasff_soon=1)
+  references_uk_soon <- c(
+    references_uk_soon,
+    dplyr::pull(df_filter, var=reference)
+    )
+  }
+# Mutate uk_rasff_soon to reflect imminent UK RASFFs and filter out.
 df_full <- dplyr::mutate(
-  df_full, uk_report_soon=dplyr::if_else(
+  df_full, uk_rasff_soon=dplyr::if_else(
     condition=df_full$reference%in%references_uk_soon,
-    true=df_full$uk_report_soon<-1,
-    false=df_full$uk_report_soon<-0
+    true=df_full$uk_rasff_soon<-1,
+    false=df_full$uk_rasff_soon<-0
     )
   )
 # Re-order the columns in the dataframe.
 col_order <- c(
   'reference', 'dateOfCase', 'date', 'month', 'days_from_start','subject',
   'type', 'classification', 'productCategory', 'product', 'hazardType',
-  'hazard', 'uk_report_soon', 'notifyingCountry', 'origin_country',
+  'hazard', 'uk_rasff_soon', 'notifyingCountry', 'origin_country',
   'dist_uk'
   )
 df_full <- df_full[, col_order] %>%
@@ -114,19 +119,57 @@ df_full <- df_full[, col_order] %>%
   dplyr::rename(product_category=productCategory) %>%
   dplyr::rename(hazard_type=hazardType) %>%
   dplyr::rename(notifying_country=notifyingCountry)
-# Fit a Bayesian network.
+# Build and fit a Bayesian network.
 df_features <- df_full %>%
-  dplyr::select(-c(reference, date_of_case, date, days_from_start)) %>%
+  dplyr::select(
+    -c(
+      reference, date_of_case, date, days_from_start, classification, type,
+      subject
+      )
+    ) %>%
   dplyr::mutate(month=as.factor(month)) %>%
-  dplyr::mutate(subject=as.factor(subject)) %>%
-  dplyr::mutate(type=as.factor(type)) %>%
-  dplyr::mutate(classification=as.factor(classification)) %>%
+  #dplyr::mutate(subject=as.factor(subject)) %>%
+  #dplyr::mutate(type=as.factor(type)) %>%
+  #dplyr::mutate(classification=as.factor(classification)) %>%
   dplyr::mutate(product_category=as.factor(product_category)) %>%
   dplyr::mutate(product=as.factor(product)) %>%
   dplyr::mutate(hazard_type=as.factor(hazard_type)) %>%
   dplyr::mutate(hazard=as.factor(hazard)) %>%
-  dplyr::mutate(uk_report_soon=as.factor(uk_report_soon)) %>%
+  dplyr::mutate(uk_rasff_soon=as.factor(uk_rasff_soon)) %>%
   dplyr::mutate(notifying_country=as.factor(notifying_country)) %>%
   dplyr::mutate(origin_country=as.factor(origin_country)) %>%
   dplyr::mutate(dist_uk=as.factor(dist_uk)) %>%
+  na.omit() %>%
   droplevels()
+df_features <- data.frame(df_features)
+## Create train and test sets.
+train_index <- sample(1:dim(df_features)[1], floor(dim(df_features)[1]*0.8))
+df_train <- df_features[train_index,]
+df_test <- df_features[-train_index,]
+## Manual structure based on expert knowledge.
+g = bnlearn::empty.graph(nodes=names(df_features))
+g = bnlearn::set.arc(x=g, from='month', to='origin_country')
+g = bnlearn::set.arc(x=g, from='origin_country', to='product')
+g = bnlearn::set.arc(x=g, from='product_category', to='product')
+g = bnlearn::set.arc(x=g, from='product', to='hazard_type')
+g = bnlearn::set.arc(x=g, from='hazard_type', to='hazard')
+g = bnlearn::set.arc(x=g, from='product', to='dist_uk')
+g = bnlearn::set.arc(x=g, from='dist_uk', to='notifying_country')
+g = bnlearn::set.arc(x=g, from='notifying_country', to='uk_rasff_soon')
+#g = bnlearn::set.arc(x=g, from='product', to='uk_rasff_soon')
+g = bnlearn::set.arc(x=g, from='hazard', to='uk_rasff_soon')
+bnviewer::viewer(bayesianNetwork=g)
+# Using strong prior.
+g_fitted_big = bnlearn::bn.fit(
+  x=g, data=df_train,
+  method='bayes',
+  iss=25*dim(df_train)[1]
+  )
+g_predicted_big <- predict(
+  g_fitted_big,
+  node='uk_rasff_soon', 
+  data=df_test
+  )
+caret::confusionMatrix(data=g_predicted_big, reference=df_test$uk_rasff_soon)
+
+
