d411032be30505df83774387fa2c9a90c94db771
tnh-fsa <52284462+tnh-fsa@users.noreply.github.com>
2020-01-21 11:29:54 +0000

    Update helpers.R
    Estimate test probabilities.

--- helpers.R
+++ helpers.R
@@ -1,7 +1,10 @@
 # Non-UK RASFF helpers.
 library(tidyverse)
+library(bnlearn)
+library(svMisc)
 source('ignore.r')
 # DB9_connection.R provides database access.
+
 
 load_rasffs <- function(columns='*', table=''){
   # The data dataframe.
@@ -17,5 +20,22 @@ load_rasffs <- function(columns='*', table=''){
   }
 
 
-
+estimate_probabilities <- function(
+  data_frame, bayesian_network, predict_column
+  ){
+    probs <- c()
+    for (r in 1:dim(data_frame)[1]){
+      per_cent_done <- round(100 * (r / dim(data_frame)[1]))
+      svMisc::progress(value=per_cent_done, progress.bar=F)
+      p <- bnlearn::cpquery(
+        fitted=bayesian_network,
+        event=(uk_rasff_soon=='1'),
+        evidence=(as.list(data_frame[r, c(-predict_column)])),
+        method='lw',
+        n=1e5
+      )
+      probs <- c(probs, p)
+    }
+    return(probs)
+    }
 
